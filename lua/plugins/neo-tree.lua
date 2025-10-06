209293f1fb7bf127a47e255aeada15f0d71bd7bb
soumik12345 <19soumik.rakshit96@gmail.com>
2025-10-06 21:09:33 +0530

    chore: unhinde hidden files and dirs in neotree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -206,9 +206,9 @@ return {
         filesystem = {
         filtered_items = {
             visible = false, -- when true, they will just be displayed differently than normal items
-            hide_dotfiles = true,
+            hide_dotfiles = false,
             hide_gitignored = true,
-            hide_hidden = true, -- only works on Windows for hidden files/directories
+            hide_hidden = false, -- only works on Windows for hidden files/directories
             hide_by_name = {
             --"node_modules"
             },
@@ -220,7 +220,7 @@ return {
             --".gitignored",
             },
             always_show_by_pattern = { -- uses glob style patterns
-            --".env*",
+            ".env*",
             },
             never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
             --".DS_Store",
