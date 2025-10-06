e8544997c36aa4a4cdb8ea032a51ccb9aff4ee1e
soumik12345 <19soumik.rakshit96@gmail.com>
2025-10-06 20:28:15 +0530

    add: alpha + gitsigns + indent-blankline

--- init.lua
+++ init.lua
@@ -26,4 +26,7 @@ require('lazy').setup({
     require('plugins.lsp'),
     require('plugins.autocompletion'),
     require('plugins.autoformatting'),
+    require('plugins.gitsigns'),
+    require('plugins.alpha'),
+	require('plugins.indent-blankline'),
 })
